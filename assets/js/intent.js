49f53751a7ac85bb17a79e8433b1dc98fbb2620f
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-20 23:56:19 +0100

    Update intent.js

--- assets/js/intent.js
+++ assets/js/intent.js
@@ -3,6 +3,5 @@ function mxplay(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.loc
 function mxplay_pro(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent:"+b+"#Intent;package=com.mxtech.videoplayer.pro;end"}};
 function extplay(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="intent:"+a+"#Intent;action=android.intent.action.VIEW;scheme=http;type=video/*;end"}};
 function intent(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent:"+b+"#Intent;action=android.intent.action.VIEW;scheme=http;type=video/mp4;end"}};
-function vlcplay(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent:"+b+"#Intent;package=org.videolan.vlc;S.title=aliwixplay.netlify.app;type=video/mp4;end"}};
-function dlna(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="wvc-x-callback://open?url="+b+"#Intent;package=com.instantbits.cast.webvideo;end"}};
-
+function vlcplay(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="vlc-x-callback://x-callback-url/stream?url=URL"+b+"&sub=aliwixplay.netlify.app"}};
+function dlna(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="wvc-x-callback://open?url="+a+"&secure_uri=true"}};
