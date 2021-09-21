b905e8d60dd3c8dc957d00428458b4f201de8128
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-21 01:50:59 +0100

    Update intent.js

--- assets/js/intent.js
+++ assets/js/intent.js
@@ -8,3 +8,11 @@ function dlna(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.locat
 
 
 
+try {
+  $('#link').attr('href', "<scheme>://<package>/?<parameters>");
+  $("#link")[0].click(); 
+} catch (error) {
+  if (confirm('You do not seem to have app installed, do you want to download it now?')) {
+    window.location = 'https://play.google.com/store/apps/details?id=123';
+  } 
+}
