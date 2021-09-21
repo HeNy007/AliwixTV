e7e3cb7c68991c69c37df352a0b413d385e29a78
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2021-09-21 04:37:54 +0100

    Update intent.js

--- assets/js/intent.js
+++ assets/js/intent.js
@@ -6,6 +6,73 @@ function intent(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.loc
 function vlcplay(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="vlc-x-callback://x-callback-url/stream?url=URL"+b+"&sub=aliwixplay.netlify.app"}};
 function dlna(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="wvc-x-callback://open?url="+a+"&secure_uri=true"}};
 
+
+function onShouldStartLoadWithRequest(request){
+
+  // short circuit these
+  if (!request.url ||
+    request.url.startsWith('http') ||
+    request.url.startsWith("/") ||
+    request.url.startsWith("#") ||
+    request.url.startsWith("javascript") ||
+    request.url.startsWith("about:blank")
+  ) {
+    return true;
+  }
+
+  // blocked blobs
+  if(request.url.startsWith("blob")){
+    Alert.alert("Link cannot be opened.");
+    return false;
+  }
+
+  // list of schemas we will allow the webview
+  // to open natively
+  if(request.url.startsWith("tel:") ||
+    request.url.startsWith("mailto:") ||
+    request.url.startsWith("maps:") ||
+    request.url.startsWith("geo:") ||
+    request.url.startsWith("sms:")
+    ){
+
+    Linking.openURL(request.url).catch(er => {
+      Alert.alert("Failed to open Link: " + er.message);
+    });
+    return false;
+  }
+
+  // let everything else to the webview
+  return true;
+}
+
+
+
+     if (!checkUrlValid(decoded_url)) {
+       Log.d("WebView","!checkUrlValid ");
+
+       boolean res=super.shouldOverrideUrlLoading(view, url);
+       if (res){
+         Log.d("WebView","res=true ");
+       }else{
+         Log.d("WebView","res=false ");
+       }
+       return  res;//false 时 不回调 RN 的 handleShouldStartLoadWithRequest 方法
+     } else {//以下是此方法的原始代码
+       Log.d("WebView","Do your special things");
+
+       // Do your special things
+       activeUrl = url;
+       dispatchEvent(  //调 RN 的 handleShouldStartLoadWithRequest 方法
+         view,
+         new TopShouldStartLoadWithRequestEvent(
+           view.getId(),
+           createWebViewEvent(view, url)));
+
+       return true;
+     }
+   }
+
+
 $(document).ready(function(){ $('#openApp').click();});
 
 
