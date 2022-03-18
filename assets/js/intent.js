45d64205e8d9a207fc587e08d806a46e1fe6e40a
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-03-18 03:32:01 +0100

    Update intent.js

--- assets/js/intent.js
+++ assets/js/intent.js
@@ -1,13 +1,3 @@
-   // Aliwixplay intent
-   function dlna2(){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=link}else{location.href="wvc-x-callback://open?url=https://"+link+"&secure_uri=true"}};
-   function wuffy(){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=link}else{location.href="intent://"+btoa("https://"+link)+"#Intent;scheme=xmtv;package=co.wuffy.player;end"}};
-   function mx(){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="intent:"+link+"#Intent;scheme=http;type=video/*;package=com.mxtech.videoplayer.ad;S.browser_fallback_url=market://details?id=com.mxtech.videoplayer.ad;S.title=AliwixPlay;end"}};
-   function vlc(){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="vlc-x-callback://x-callback-url/stream?url=https://"+link+"&sub=AliwixPlay"}};
-   function aliwixplay(){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=link}else{location.href="http://raw.githack.com/HeNy007/AliwixPlayer/main/plyr.html?src=https://"+link}};
-   function http(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-1.html?src="+a"&sub=AliwixPlay"}};
-   function https(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-2.html?src="+a"&sub=AliwixPlay"}};
-   function userhtpp(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-3.html?src="+a"&sub=AliwixPlay"}};
-   function userhttps(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-4.html?src="+a"&sub=AliwixPlay"}};
 function wuffyplay(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent://"+btoa(b)+"#Intent;scheme=xmtv;package=co.wuffy.player;end"}};
 function mxplay(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="intent:"+a+"#Intent;package=com.mxtech.videoplayer.ad;end"}};
 function mxplay_pro(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent:"+b+"#Intent;package=com.mxtech.videoplayer.pro;end"}};
@@ -15,10 +5,7 @@ function extplay(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.lo
 function intent(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="intent:"+b+"#Intent;action=android.intent.action.VIEW;scheme=http;type=video/mp4;end"}};
 function vlcplay(b){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=b}else{location.href="vlc-x-callback://x-callback-url/stream?url=URL"+b+"&sub=aliwixplay.netlify.app"}};
 function dlna(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="wvc-x-callback://open?url="+a+"&secure_uri=true"}};
-
-<!--
-document.write('<br>your resolution is'.screen.width+'x'+screen.height)
-//-->
-
-
-
+function http(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-1.html?src="+a+"&sub=AliwixPlay"}};
+function https(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-2.html?src="+a+"&sub=AliwixPlay"}};
+function userhtpp(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-3.html?src="+a+"&sub=AliwixPlay"}};
+function userhttps(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwix-lite.blogspot.com/p/player-4.html?src="+a+"&sub=AliwixPlay"}};
