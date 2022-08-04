7fdb1cce1084c6c98be039430fab387d87b76e1e
HeNy Bn-Hamed <59735422+HeNy007@users.noreply.github.com>
2022-08-04 18:08:40 +0100

    Update intent.js

--- assets/js/intent.js
+++ assets/js/intent.js
@@ -10,3 +10,4 @@ function http(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.locat
 function https(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwixplay.tn/player/aplays?src="+btoa(a)}};
 function http1(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwixplay.tn/player/aplay1?src="+btoa(a)}};
 function https1(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwixplay.tn/player/aplays1?src="+btoa(a)}};
+function radio(a){if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){window.location=a}else{location.href="https://aliwixplay.tn/player/radio?src="+btoa(a)}};
